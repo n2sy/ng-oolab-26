62a51e55cf295e69557c9b12a5e691405b5f4eef
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2026-03-14 13:03:30 +0100

    ajout du modèle candidat

--- src/app/cv/cv.ts
+++ src/app/cv/cv.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Liste } from '../liste/liste';
 import { Details } from '../details/details';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
@@ -8,4 +9,10 @@ import { Details } from '../details/details';
   templateUrl: './cv.html',
   styleUrl: './cv.css',
 })
-export class Cv {}
+export class Cv {
+  allCandidates: Candidat[] = [
+    new Candidat(1, 'bart', 'simpson', 26, 'ingénieur', 'bart.jpeg'),
+    new Candidat(2, 'homer', 'simpson', 53, 'chef de projet', 'homer.png'),
+    new Candidat(3, 'lisa', 'simpson', 21, 'designer', 'lisa.png'),
+  ];
+}
---
+++ src/app/models/candidat.ts
@@ -0,0 +1,22 @@
+export class Candidat {
+  constructor(
+    public id: number,
+    public prenom: string,
+    public nom: string,
+    public age: number,
+    public profession: string,
+    public avatar: string,
+  ) {}
+}
+
+// export class Candidat {
+//   id: number;
+//   prenom: string;
+//   nom: string;
+
+//   constructor(id: number, prenom: string, nom: string) {
+//     this.id = id;
+//     this.prenom = prenom;
+//     this.nom = nom;
+//   }
+// }
